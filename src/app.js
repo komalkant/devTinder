23fbe72425c015286e8596825f46f64508478476
Komalkant Gupta <gupta.komalkant@thinksys.com>
2025-08-26 00:45:40 +0530

    Learn route handler

--- src/app.js
+++ src/app.js
@@ -2,47 +2,48 @@ const express = require('express');
 
 const app = express();
 
-app.get("/user", (req, res) => {
-    // Params
-    // console.log(req.params);
-    // query
-    // console.log(req.query);
-    res.send({firstName: 'komalkant', lastName: 'Gupta'})
+app.get("/user", (req, res, next) => {
+    console.log("Route handler1")
+    next();
+    //res.send('response send') // if you passingg next() function then res.send only workin last rote handler
+},[(req, res, next) => { // for multiple Route handler need to pass next() function
+    console.log("Route handler2")
+    next();
+    // res.send('response send2')
+},, (req, res, next) => { // for multiple Route handler need to pass next() function
+    console.log("Route handler3")
+    next();
+    // res.send('response send2')
+}], (req, res) => {
+    console.log("Route handler4")
+    res.send('response send4')
 })
 
-app.post("/user", (req, res) => {
-    console.log("Data saved succesfully");
-    res.send("saved");
-})
+// app.post("/user", (req, res) => {
+//     console.log("Data saved succesfully");
+//     res.send("saved");
+// })
 
-app.patch("/user", (req, res) => {
-    console.log("Data Updated succesfully with patch");
-})
-
-app.put("/user", (req, res) => {
-    console.log("Data Updated succesfully with put");
-})
-
-app.delete("/user", (req, res) => {
-    console.log("Data Deleted");
-    res.send("data deleted");
-})
-
-app.use("/hello/23",(req, res) => {
-    res.send("Hello from hello Route/23 ");
-})
-
-app.use("/hello",(req, res) => {
-    res.send("Hello from hello route ");
-})
-
-app.use("/test", (req, res) => {
-    res.send("Hello from Server");
-})
 
 // app.use("/",(req, res) => {
 //     res.send("Hello from Root get request");
 // })
+
+const cb0 = function (req, res, next) {
+  console.log('CB0')
+  next()
+}
+
+const cb1 = function (req, res, next) {
+  console.log('CB1')
+  next()
+}
+
+const cb2 = function (req, res) {
+  res.send('Hello from C!')
+}
+
+app.get('/example/c', [cb0, cb1, cb2])
 
 app.listen(5151, () => {
     console.log("server is Successfully running on the 5151 Port");
