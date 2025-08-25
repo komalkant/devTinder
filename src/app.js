4da2e20118590ec7d3bbd899343bd28086c7c32e
Komalkant Gupta <gupta.komalkant@thinksys.com>
2025-08-25 16:54:57 +0530

    play with route

--- src/app.js
+++ src/app.js
@@ -2,17 +2,47 @@ const express = require('express');
 
 const app = express();
 
-app.use("/hello",(req, res) => {
-    res.send("Hello from nodemon ");
+app.get("/user", (req, res) => {
+    // Params
+    // console.log(req.params);
+    // query
+    // console.log(req.query);
+    res.send({firstName: 'komalkant', lastName: 'Gupta'})
 })
 
-app.use("/",(req, res) => {
-    res.send("Hello from Root get request");
+app.post("/user", (req, res) => {
+    console.log("Data saved succesfully");
+    res.send("saved");
+})
+
+app.patch("/user", (req, res) => {
+    console.log("Data Updated succesfully with patch");
+})
+
+app.put("/user", (req, res) => {
+    console.log("Data Updated succesfully with put");
+})
+
+app.delete("/user", (req, res) => {
+    console.log("Data Deleted");
+    res.send("data deleted");
+})
+
+app.use("/hello/23",(req, res) => {
+    res.send("Hello from hello Route/23 ");
+})
+
+app.use("/hello",(req, res) => {
+    res.send("Hello from hello route ");
 })
 
 app.use("/test", (req, res) => {
     res.send("Hello from Server");
 })
+
+// app.use("/",(req, res) => {
+//     res.send("Hello from Root get request");
+// })
 
 app.listen(5151, () => {
     console.log("server is Successfully running on the 5151 Port");
